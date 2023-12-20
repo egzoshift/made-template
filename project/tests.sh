554ee24eb647bc9f16631234c009b89f94144fb2
egzoshift <75319994+egzoshift@users.noreply.github.com>
2023-12-20 02:20:18 +0100

    Update tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 python3 ./automatedPipeline.py
-echo "Pipeline executed v5"
+echo "Pipeline executed"
 
 python3 ./test.py
-echo "Pipeline tested v5"
+echo "Pipeline tested"
 
 # To run the tests you need kaggle credentials to pull data from kaggle.
 # To do so: 
